753bfc9a07f9df530b1ed5cb9dfb33fc27a8d2b1
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2023-11-17 16:25:53 +0000

    after the meeting

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -3,15 +3,16 @@ const createRouter = function (collection) {
   const router = express.Router();
   //GET ALL
   router.get("/", (request, response) => {
-    collection
-      .find()
-      .toArray()
-      .then((docs) => response.json(docs))
-      .catch((err) => {
-        console.error(err);
-        response.status(500);
-        response.json({ status: 500, error: err });
-      });
+    // collection
+    //   .find()
+    //   .toArray()
+    //   .then((docs) => response.json(docs))
+    //   .catch((err) => {
+    //     console.error(err);
+    //     response.status(500);
+    //     response.json({ status: 500, error: err });
+      // });
+    response.send('hello');
   });
  
   //GET ONE
